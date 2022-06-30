c9eb09bfdc9395b7bc5779c8a820f95db8e7275d
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-06-30 00:31:54 -0400

    projects update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,42 +8,50 @@ const Projects = () => (
       <div className="projects-content">
         <h2>Projects</h2>
         <h3>
+          <a>Revix</a>
+        </h3>
+        <p className="p-content">
+          Designing a model that produces electronic dance music similar to your
+          favorite artists, using a multi-scale VQ-VAE and autoregressive
+          transformers. Trained on music samples of various genres and artists,
+          the model aims to produce new samples of the user's liking
+        </p>
+        <h3>
           <a href="https://devpost.com/software/daymaker">DayMaker</a>
         </h3>
         <p className="p-content">
-          A web application that automatically creates Google Calendar events by
-          extracting relevant titles and corresponding dates from uploaded
-          documents using Google Cloud's DocumentAI and NLP API. This project
-          won the <b>Best Use of Google Cloud API</b> challenge at HackTX 2021.
-          Our team is currently working towards building DayMaker from scratch
-          without Google's services.
+          Full-stack web application that automatically creates Google Calendar
+          events from uploaded documents, using a custom trained NER model to
+          extract dates and relevant titles. The project was awarded Best Use of
+          Google Cloud 1st Place at HackTX 2021, and our team is currently
+          working on building DayMaker without Google's services
         </p>
         <h3>
           <a href="https://github.com/thaneesh-babu/Caproxy">Caproxy</a>
         </h3>
         <p className="p-content">
-          A blockchain application that allows users to perform transactions on
-          the Ethereum network, with a React frontend, using Solidity smart
-          contracts and Metamask to interact with the blockchain.
+          Blockchain application that allows users to perform transactions on
+          the Ethereum network, using a React frontend, Solidity smart contracts
+          and MetaMask to interact with the blockchain
         </p>
         <h3>
           <a href="https://github.com/thaneesh-babu/DreddIT">DreddIT</a>
         </h3>
         <p className="p-content">
-          Performs sentiment analysis on the comments of a user-entered number
-          of posts within a searched subreddits and subsequently generates plots
-          describing the frequency and sentiment of words, displayed on a React
-          application served by a Flask backend.
+          NLP model that performs sentiment analysis on the comments of a
+          user-entered number of posts within a searched subreddit, generating
+          plots describing the subreddit's sentiment over numerous features,
+          rendered on a React application and backed by a Flask server
         </p>
         <h3>
           <a href="https://github.com/thaneesh-babu/Might-Bite">Might Bite</a>
         </h3>
         <p className="p-content">
-        Built an application that displays recipes made with matching user-entered
-                ingredients with a GUI using Swing. Returned recipes were categorized with vegan, vegetarian and
-                lactose-free labels
-                for a comprehensive list of recommendations. The application improved dining hall’s efficiency by 200% and was used
-                by 20+ faculty members & 50+ students.
+          Built an application that displays recipes made with matching
+          user-entered ingredients, responsible for the project's backend
+          development; used a cloud hosted SQL database to store 50+ recipes.
+          Integrated the application with the school dining hall, improving
+          efficiency by 200% and used by 20+ faculty members
         </p>
       </div>
     </div>
