030290120ae6727a4f72e5a7cf28e37f0ed1982e
Thaneesh Babu <gkthaneesh2@gmail.com>
2023-06-10 23:52:12 -0400

    add education

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,17 +11,41 @@ const About = () => (
     mx="auto"
     paddingTop={["5%", "10%", "2.5%"]}
   >
-    <Text fontSize="xl" align={"left"} paddingTop={3}>
-      I'm a junior at Georgia Tech, majoring in Computer Science specializing in
-      Artificial Intelligence and Systems/Architecture. I take interest in
-      full-stack development, machine learning, systems and computer
-      architecture.
-    </Text>
-    <Text fontSize="xl" align={"left"} paddingTop={3}>
-      Besides these interests, I'm huge into football (the real one),
-      astrophysics, and listening to EDM.
-    </Text>
-
+    <Box boxShadow="lg" p="6" rounded="md">
+      <Text fontSize="xl" align={"left"} paddingTop={3}>
+        I'm a junior at Georgia Tech, majoring in Computer Science specializing
+        in Artificial Intelligence and Systems/Architecture. I take interest in
+        full-stack development, machine learning, systems and computer
+        architecture.
+      </Text>
+      <Text fontSize="xl" align={"left"} paddingTop={3}>
+        Besides these interests, I'm huge into football (the real one),
+        astrophysics, and listening to EDM.
+      </Text>
+    </Box>
+    <Box boxShadow="lg" p="6" rounded="md" marginTop={10}>
+      <HStack spacing="275px">
+        <Text fontSize="xl" align={"left"} fontWeight="bold">
+          {" "}
+          Georgia Institute of Technology{" "}
+        </Text>
+        <Text fontSize="lg"> Aug 2021 - Dec 2024 </Text>
+      </HStack>
+      <Text fontSize="lg" align={"left"} fontWeight="medium">
+        {" "}
+        B.S. in Computer Science, Cumulative GPA: 3.97, Major GPA: 4.0{" "}
+      </Text>
+      <Text fontSize="md" align={"left"} fontWeight="medium">
+        {" "}
+        <b>Concentrations</b>: Artificial Intelligence, Systems/Architecture{" "}
+      </Text>
+      <Text fontSize="md" align={"left"} fontWeight="medium">
+        {" "}
+        <b>Coursework</b>: Algorithms Honors, Data Structures, Operating
+        Systems, Artificial Intelligence, Systems & Networks, Linear Algebra,
+        Applied Combinatorics, Discrete Mathematics, Statistics & Applications{" "}
+      </Text>
+    </Box>
   </Box>
 );
 
